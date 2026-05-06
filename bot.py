8df6831853cfd104a82cf8edd5d96a3834f20a8d
Express <express@example.com>
2026-05-07 00:46:06 +0300

    daily message

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
 import os
 import asyncio
 from telegram import Bot
+from datetime import datetime
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=TOKEN)
 
+TARGET_HOUR = 12  # الساعة 12
+TARGET_MINUTE = 0
+
 async def main():
-    print("✅ البوت بدأ")
+    print("✅ البوت شغال")
 
     while True:
-        try:
-            await bot.send_message(chat_id=CHAT_ID, text="🔥 البوت شغال 100%")
-            print("📩 تم إرسال رسالة")
-        except Exception as e:
-            print("❌ Error:", e)
+        now = datetime.now()
 
-        await asyncio.sleep(60)  # كل دقيقة
+        if now.hour == TARGET_HOUR and now.minute == TARGET_MINUTE:
+            try:
+                await bot.send_message(chat_id=CHAT_ID, text="🔥 الرسالة اليومية")
+                print("📩 تم الإرسال")
+                await asyncio.sleep(60)  # يمنع التكرار
+            except Exception as e:
+                print("❌ Error:", e)
+
+        await asyncio.sleep(30)
 
 asyncio.run(main())
