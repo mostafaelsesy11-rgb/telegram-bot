fbe5d62dce02848fbb19f99d8b1099ca8d750e00
Express <express@example.com>
2026-05-07 00:25:44 +0300

    update bot test

--- bot.py
+++ bot.py
@@ -1,42 +1,12 @@
-import asyncio
-import datetime
-import os
 from telegram import Bot
+import asyncio
 
-TOKEN = "8623245671:AAGwJlyDJTQMh_irr2nKKXfhTlSsHRucV0E"
-CHAT_ID = "6063339002"
-VIDEO = "reel_pro_plus.mp4"
+TOKEN = "8791016523:AAFgWjFc1Ob9HNCtb_zopTN3JyrWnvVWjbU"
+CHAT_ID = 6063339002
 
 bot = Bot(token=TOKEN)
 
-async def send_video():
-    await bot.send_video(
-        chat_id=CHAT_ID,
-        video=open(VIDEO, "rb"),
-        caption="📖 آية قرآنية يومية"
-    )
+async def main():
+    await bot.send_message(chat_id=CHAT_ID, text="🔥 شغال")
 
-def seconds_until(hour, minute):
-    now = datetime.datetime.now()
-    target = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
-
-    if target <= now:
-        target += datetime.timedelta(days=1)
-
-    return (target - now).total_seconds()
-
-async def job():
-    while True:
-        wait_time = seconds_until(14, 20) # 
-        print(f"⏳ انتظار {int(wait_time)} ثانية لميعاد الإرسال...")
-        await asyncio.sleep(wait_time)
-
-        print("🎬 تجهيز الفيديو...")
-        os.system("py pro_video.py")
-
-        print("📤 إرسال الفيديو...")
-        await send_video()
-
-        print("✅ تم الإرسال اليومي بنجاح")
-
-asyncio.run(job())
+asyncio.run(main())
